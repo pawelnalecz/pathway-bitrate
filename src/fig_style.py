8a23622e4b8590bd3816a035db999b6004ad5fd2
pawelnalecz <pawel.nalecz-jawecki@ippt.pan.pl>
2025-09-09 16:15:07 +0200

    Add backup font for arrows

--- src/fig_style.py
+++ src/fig_style.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 
-plt.rcParams["font.sans-serif"] = ['Helvetica']
-plt.rcParams["font.family"] = 'Helvetica'
+plt.rcParams["font.sans-serif"] = ['Helvetica', 'Arial']
+plt.rcParams["font.family"] = ['Helvetica', 'Arial']
 plt.rcParams['font.size'] = 8
 plt.rcParams['svg.fonttype'] = 'none'
 plt.rcParams['mathtext.fontset'] = 'custom'
